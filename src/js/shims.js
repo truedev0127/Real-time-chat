ab7feeaf5821e017dd56d8e4eedfe26b90eefef7
TopTalent <danish.hakim0127@gmail.com>
2026-01-31 08:12:35 +0900

    Update shims.js

--- src/js/shims.js
+++ src/js/shims.js
@@ -7,13 +7,13 @@
         throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
       }
   
-      var aArgs = Array.prototype.slice.call(arguments, 1), 
-          fToBind = this, 
-          fNOP = function() {},
-          fBound = function() {
-            return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
-                                 aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
+      // var aArgs = Array.prototype.slice.call(arguments, 1), 
+      //     fToBind = this, 
+      //     fNOP = function() {},
+      //     fBound = function() {
+      //       return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+      //                            aArgs.concat(Array.prototype.slice.call(arguments)));
+      //     };
    
       fNOP.prototype = this.prototype;
       fBound.prototype = new fNOP();
